554754a9d26d3d33af30b74efe65eb56ea5055ec
Mehrnegar Rezaie <146884643+mehrnegarr@users.noreply.github.com>
2023-10-16 10:57:13 +0330

    Update HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,8 +1,12 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:recipe_rover/RecipeCard.dart';
-import 'HomeScreen.dart'; // Import your HomeScreen
+import 'HomeScreen.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +28,7 @@ class HomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
+            const Text(
               'Welcome to our RecipeRover App',
               style: TextStyle(
                 fontSize: 24.0,
@@ -33,24 +37,21 @@ class HomePage extends StatelessWidget {
               ),
               textAlign: TextAlign.center,
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             Image.asset(
               'assets/images.jpg',
               height: 470,
               fit: BoxFit.cover,
             ),
-            SizedBox(height: 20.0),
-
-            // Explore Recipes Button
-            Container(
-              width: double.infinity, // Adjust the width as needed
+            const SizedBox(height: 20.0),
+            SizedBox(
+              width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-                  // Navigate to RecipeScreen when Explore Recipes is clicked
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => RecipeScreen(),
+                      builder: (context) => const RecipeScreen(),
                     ),
                   );
                 },
@@ -59,21 +60,18 @@ class HomePage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(30.0),
                   ),
                 ),
-                child: Text('Explore Our Recipes'),
+                child: const Text('Explore Our Recipes'),
               ),
             ),
-            SizedBox(height: 20.0),
-
-            // Add Recipe Button
-            Container(
-              width: double.infinity, // Adjust the width as needed
+            const SizedBox(height: 20.0),
+            SizedBox(
+              width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-                  // Navigate to HomeScreen when Add Your Recipe is clicked
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => HomeScreen(),
+                      builder: (context) => const HomeScreen(),
                     ),
                   );
                 },
@@ -82,7 +80,7 @@ class HomePage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(30.0),
                   ),
                 ),
-                child: Text('Add Your Recipe'),
+                child: const Text('Add Your Recipe'),
               ),
             ),
           ],
